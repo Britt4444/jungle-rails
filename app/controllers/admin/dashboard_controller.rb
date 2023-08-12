4ffe85b510a6aed50257c5a151f9af8e81dc534c
Brittany Cowper <brittferren@gmail.com>
2023-08-12 18:16:20 -0500

    add HTTP basic auth to admin pages, store relevant variables in .env

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'].to_s, password: ENV['ADMIN_PASSWORD'].to_s
+  
   def show
+    @ProductsCount = Product.all.count
+    @CategoriesCount = Category.all.count
   end
 end
