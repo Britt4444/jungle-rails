b45784d6676504bb63c3f72ba1c9554760cd9b21
Brittany Cowper <brittferren@gmail.com>
2023-08-19 14:22:44 -0500

    add pry to gems

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
+    # require 'pry'
+    # binding.pry
     if user.save
       session[:user_id] = user.id
       redirect_to '/'
