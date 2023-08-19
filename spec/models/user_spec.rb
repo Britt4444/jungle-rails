fe9158aef47ff95104134ca5b485eaca5230ed2e
Brittany Cowper <brittferren@gmail.com>
2023-08-19 14:43:41 -0500

    WIP: write rspec tests for User, 2 currently failing

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,118 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    it 'Includes all fields and saves successfully' do
+      @category = Category.create(:name => "test")
+      user =  User.new(
+        :first_name => "first name",
+        :last_name => "last name",
+        :email => "email@email.com",
+        :password => "1234",
+        :password_confirmation => "1234")
+
+      user.validate
+      # should save without error msg
+      expect(user.errors.full_messages).to be_empty
+    end
+
+    it 'Validates presence of password' do
+      @category = Category.create(:name => "test")
+      user =  User.new(
+        :first_name => "first name",
+        :last_name => "last name",
+        :email => "email@email.com",
+        :password => nil,
+        :password_confirmation => "1234")
+
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to include "Password can't be blank"
+    end
+
+    it 'Validates presence of password' do
+      @category = Category.create(:name => "test")
+      user =  User.new(
+        :first_name => "first name",
+        :last_name => "last name",
+        :email => "email@email.com",
+        :password => "1234",
+        :password_confirmation => nil)
+
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to include "Password confirmation can't be blank"
+    end
+
+    it 'Validates that password and password confirmation match' do
+      @category = Category.create(:name => "test")
+      user =  User.new(
+        :first_name => "first name",
+        :last_name => "last name",
+        :email => "email@email.com",
+        :password => "password",
+        :password_confirmation => "1234")
+
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to include "Password confirmation doesn't match Password"
+    end
+
+#### failing
+    it 'Validates the uniqueness of email' do
+      @category = Category.create(:name => "test")
+      user =  User.new(
+        :first_name => "first name",
+        :last_name => "last name",
+        :email => "email@email.com",
+        :password => "1234",
+        :password_confirmation => "1234")
+      
+      user_two =  User.new(
+        :first_name => "first",
+        :last_name => "last",
+        :email => "EMAIL@email.com",
+        :password => "password",
+        :password_confirmation => "password")
+
+      expect(user_two).to_not be_valid
+      expect(user_two.errors.full_messages).to include "Email has already been taken"
+    end
+
+    it 'Validates presence of first name' do
+      @category = Category.create(:name => "test")
+      user =  User.new(
+        :first_name => nil,
+        :last_name => "last name",
+        :email => "email@email.com",
+        :password => "1234",
+        :password_confirmation => "1234")
+
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to include "First name can't be blank"
+    end
+
+    it 'Validates presence of last name' do
+      @category = Category.create(:name => "test")
+      user =  User.new(
+        :first_name => "first name",
+        :last_name => nil,
+        :email => "email@email.com",
+        :password => "1234",
+        :password_confirmation => "1234")
+
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to include "Last name can't be blank"
+    end
+#### failing
+    it 'Validates presence of email' do
+      @category = Category.create(:name => "test")
+      user =  User.new(
+        :first_name => "first name",
+        :last_name => "last name",
+        :email => nil,
+        :password => "1234",
+        :password_confirmation => "1234")
+
+      expect(user).to_not be_valid
+      expect(user.errors.full_messages).to include "Email can't be blank"
+    end
+  end
+end
